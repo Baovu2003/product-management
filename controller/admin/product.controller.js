554f65525d197471311bf3d95573959851eabe6a
Baovu2003 <vuluongbao.qc2003@gmail.com>
2024-09-07 01:04:52 +0700

    Load sản phẩm ở bên trang admin

--- controller/admin/product.controller.js
+++ controller/admin/product.controller.js
@@ -1,8 +1,16 @@
 // [GET]: /admin/products
 
-module.exports.index = (req, res) => {
+const Product = require("../../models/product.model")
+
+
+
+module.exports.index = async (req, res) => {
+  const products = await Product.find({
+    deleted: false
+  })
     res.render("admin/pages/products/index.pug",{
-      pageTitle:"Danh sách sản phẩm"
+      pageTitle:"Danh sách sản phẩm",
+      products: products
     });
     // res.send("Trang sản phẩm")
   }
