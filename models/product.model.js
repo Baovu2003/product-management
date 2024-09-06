987dddf86946781c65f157c3d1917594cbcfbcb9
Baovu2003 <vuluongbao.qc2003@gmail.com>
2024-09-06 10:25:05 +0700

    first commit

---
+++ models/product.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  price: Number,
+  discountPercentage: Number,
+  stock: Number,
+  thumbnail: String,
+  status: String,
+  position: Number,
+  deleted: Boolean,
+});
+// Tham số thứ 3 trongt phần này là tên của collection trong database product-management
+const Product = mongoose.model("Product", productSchema, "products");
+module.exports = Product;
