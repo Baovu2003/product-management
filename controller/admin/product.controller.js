0f78135c77ae3b6391534737348e773453e070d2
Baovu2003 <vuluongbao.qc2003@gmail.com>
2024-09-10 00:06:05 +0700

    pagination

--- controller/admin/product.controller.js
+++ controller/admin/product.controller.js
@@ -9,7 +9,6 @@ module.exports.index = async (req, res) => {
 
   // Đoạn bộ lọc
   const filterStatus = filterStatusHelper(req.query)
-
   let find = {
     deleted: false,
   };
@@ -19,22 +18,45 @@ module.exports.index = async (req, res) => {
   }
 
     // Đoạn search
-
    // Sử dụng searchHelper để xử lý logic tìm kiếm
   const objectSearch = searchHelper(req.query,find)
   console.log("objectSearch:",objectSearch);
-
   // Nếu có regex từ objectSearch (tức là có từ khóa tìm kiếm)
   if (objectSearch.regex) {
-    
     find.title = objectSearch.regex; // Thêm điều kiện tìm kiếm vào find
   }
 
-  const products = await Product.find(find);
+  // PAGINATION
+
+    let objectPagination = {
+      currentPage: 1,
+      limitItems: 4
+    }
+
+    if(req.query.page){
+      objectPagination.currentPage = Number(req.query.page) || 1
+    }
+
+    objectPagination.skip = (objectPagination.currentPage-1) * objectPagination.limitItems
+    console.log( objectPagination.currentPage)
+
+
+    // Đếm tổng số lượng product trong db
+    const countProduct = await Product.countDocuments(find);
+    console.log("countProduct",countProduct)
+
+    // Tính số lượng page
+    const totalPage = Math.ceil(countProduct/objectPagination.limitItems)
+    console.log("totalPage",totalPage)
+    objectPagination.totalPage = totalPage
+    
+  // End
+  const products = await Product.find(find).limit(4).skip(objectPagination.skip);
   res.render("admin/pages/products/index.pug", {
     pageTitle: "Danh sách sản phẩm",
     products: products,
     filterStatus: filterStatus,
     keyword: objectSearch.keyword,
+    pagination: objectPagination,
   });
 };
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -1,14 +1,12 @@
 console.log("Đã vào file script.js bên admin");
 
-// Xử lý button status
-
+// ---------------------Xử lý button status-----------------------
 // Đầu tiên lấy ra hết các button status
 const buttonStatus = document.querySelectorAll("[button-status]");
 console.log(buttonStatus);
 if (buttonStatus.length > 0) {
-
-    let url = new URL( window.location.href);
-    console.log("url",url)
+  let url = new URL(window.location.href);
+  console.log("url", url);
 
   buttonStatus.forEach((button) => {
     console.log(button);
@@ -16,39 +14,52 @@ if (buttonStatus.length > 0) {
       const status = button.getAttribute("button-status");
       console.log(status);
 
-      if(status){
-        url.searchParams.set("status", status)
-      }else{
-        url.searchParams.delete("status")
+      if (status) {
+        url.searchParams.set("status", status);
+      } else {
+        url.searchParams.delete("status");
       }
-      console.log("url.href",url.href)
+      console.log("url.href", url.href);
 
-    //Sau khi chỉnh sửa URL,
-    //trang web sẽ được điều hướng (redirect) đến URL mới với trạng thái vừa chọn.
-      window.location.href = url.href
+      //Sau khi chỉnh sửa URL,
+      //trang web sẽ được điều hướng (redirect) đến URL mới với trạng thái vừa chọn.
+      window.location.href = url.href;
     });
   });
 }
+
+// --------------------------------Phần search--------------------------
+
 //Nếu không xử lý bên dưới thì khi không nhập gì nó sẽ hiện ra cái này:
 // http://localhost:3000/admin/products?keyword=
 
 // Xử lý cái này để có thể xoá đc cái keyword trên thanh url khi không nhập gì
+// http://localhost:3000/admin/products
+const formSeach = document.querySelector("#form-search");
+console.log(formSeach);
+if (formSeach) {
+  let url = new URL(window.location.href);
+  formSeach.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(e.target.elements.keyword.value);
+    const keyword = e.target.elements.keyword.value;
+    if (keyword) {
+      url.searchParams.set("keyword", keyword);
+    } else {
+      url.searchParams.delete("keyword");
+    }
+    window.location.href = url.href;
+  });
+}
 
- const formSeach = document.querySelector("#form-search");
- console.log(formSeach)
- if(formSeach){
-    let url = new URL( window.location.href);
-    formSeach.addEventListener("submit", (e) =>{
-        e.preventDefault();
-        console.log(e.target.elements.keyword.value)
-        const keyword = e.target.elements.keyword.value;
-        if(keyword){
-            url.searchParams.set("keyword", keyword)
-        }else{
-            url.searchParams.delete("keyword")
-          }
-          window.location.href = url.href
-    })
+//-------------------------------Phần PAGINATION------------------------------
+const buttonPagination = document.querySelectorAll("[button-pagination]");
 
-
- }
+let url = new URL(window.location.href);
+buttonPagination.forEach((button) => {
+  button.addEventListener("click", () => {
+    const pagination = button.getAttribute("button-pagination");
+    url.searchParams.set("page", pagination);
+    window.location.href = url.href;
+  });
+});
