acef3f2b4318b4aff8a3b6fe636d0ab308823c9e
Baovu2003 <vuluongbao.qc2003@gmail.com>
2024-09-15 16:27:57 +0700

    preview ảnh khi chọn

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -193,3 +193,17 @@ if(showAlert){
   console.log("time",time)
   console.log("OKKKKKKK")
 }
+
+// ----Upload-image---------
+const uploadImage = document.querySelector("[upload-image]");
+if(uploadImage){
+  const uploadImageInput = document.querySelector("[upload-image-input]");
+  const uploadImagePreview = document.querySelector("[upload-image-preview]");
+  uploadImageInput.addEventListener("change",(e)=>{
+    console.log(e.target)
+    const file = e.target.files[0];
+    if(file){
+      uploadImagePreview.src = URL.createObjectURL(file)
+    }
+  })
+}
